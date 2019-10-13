a7a3ddac2d89db4a8b764fbb5cd6bf32807fe4d7
mshoaibnoor <muhammad.shoaib.masood@gmail.com>
2019-10-14 00:20:17 +0500

    third change

--- hello_world/app.py
+++ hello_world/app.py
@@ -36,7 +36,7 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "hello shoaib",
+            "message": "hello shoaib123",
             # "location": ip.text.replace("\n", "")
         }),
     }
